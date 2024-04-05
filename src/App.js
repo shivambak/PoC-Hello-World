2bcfce9b2e29d5c781cb18300fcd09bbb4b535cb
shivambak <shivambakshi67@gmail.com>
2024-04-05 01:52:53 -0700

    s

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import image from "./1664006918432.jpeg";
+import rock from "./rock-one-eyebrow-raised-rock-staring.gif";
 
 function App() {
+  const interestList = ['Going to the gym', 'Watching F1', 'Cars', 'Sleeping'];
+
+  // State to store the list
+  const [list, setList] = useState(interestList);
+
+  // Function to randomize the order of the list using sort
+  const randomizeList = () => {
+    const newList = [...list].sort(() => Math.random() - 0.5);
+    setList(newList);
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Welcome to Shivam Bakshi's CSS 480 webpage!
+      </h1>
+      <br/>
+      <img src= {image} alt= "Shivam Bakshi" style={{width: '200px'}} ></img>
+      <img src= {rock} alt= "Shivam Bakshi" style={{width: '200px'}} ></img>
+      <br/>
+      
+      <p>I'm a fourth year UW Bothell student that is majoring in Computer Science. 
+        A little fun about me is that I am one of two people in my entire family that is left handed.</p>
+     
+      <div className='h3'>
+        <h3>Somethings that interest me include:</h3>
+      </div> 
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <button className='button' onClick={randomizeList}>Reorder List</button>
     </div>
   );
 }
